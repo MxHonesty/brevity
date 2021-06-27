287faab0e2934aad24ba521a7f91d5a966decabb
Stelian <stoianstelian33@gmail.com>
2021-06-27 19:05:03 +0300

    added mock table widget

--- ui/ui.go
+++ ui/ui.go
@@ -21,17 +21,20 @@ type UI struct {
 
 	MainHBox *fyne.Container  // The Main Layout that has to be displayed.
 	SideMenuVBox *fyne.Container
-	CalendarBox *fyne.Container
+	TableVBox *fyne.Container
+	CalendarBox *widget.Table
 }
 
 // Method for setting up the layouts.
 func (ui *UI) setUpLayout() {
 	ui.MainHBox = container.NewHBox()  // The main Layout for the ui.
 	ui.SideMenuVBox = container.NewVBox()
-	ui.CalendarBox = container.NewGridWithColumns(7)
+	ui.TableVBox = container.NewVBox()
+
+	ui.TableVBox.Add(ui.CalendarBox)
 
 	ui.MainHBox.Add(ui.SideMenuVBox)
-	ui.MainHBox.Add(ui.CalendarBox)
+	ui.MainHBox.Add(ui.TableVBox)
 }
 
 // Method for initializing the content inside the layouts.
@@ -39,11 +42,21 @@ func (ui *UI) initContent() {
 	hello := widget.NewLabel("Hello Brevity!")
 	hello2 := widget.NewLabel("Hello Brevity!")
 	hello3 := widget.NewLabel("Hello Brevity!")
-	hello4 := widget.NewLabel("Calendar starts here")
 	ui.SideMenuVBox.Add(hello)
 	ui.SideMenuVBox.Add(hello2)
 	ui.SideMenuVBox.Add(hello3)
-	ui.CalendarBox.Add(hello4)
+}
+
+// Creates a table full of test values for Labels.
+func (ui *UI) initTable() {
+	ui.CalendarBox = widget.NewTable(
+		func() (int, int) {return 7, 8},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("TEST")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText("TEST1")
+		})
 }
 
 // Create a new instance of UI. Takes as argument a service.AbstractServiceFactory.
@@ -59,6 +72,8 @@ func NewUI(srvFactory service.AbstractServiceFactory) *UI {
 func (ui *UI) Run() {
 	a := app.New()
 	w := a.NewWindow("Hello")
+
+	ui.initTable()
 	ui.setUpLayout()
 	ui.initContent()
 
